e997d6bb5810a0eaa6fda33aa5de15a44185e7d8
Jupyo Seo <jpseo84@gmail.com>
2023-05-18 21:58:23 +0900

    Added unicode checking

--- extractor.pl
+++ extractor.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use utf8;
 use Time::HiRes qw(time);
+use Encode::Guess;
 
 sub process_file {
     my ($target_file_path, $dictionary_file_path) = @_;
@@ -21,17 +22,35 @@ sub process_file {
     $output_file_path .= '_output.txt';
     open my $output_file, '>:encoding(UTF-8)', $output_file_path or die "Cannot open output file: $!";
 
-    open my $target_file, '<:encoding(UTF-8)', $target_file_path or die "Cannot open target file: $!";
-    while (my $line = <$target_file>) {
-        if (grep { $dictionary{lc $_} } split /[\W_]+/u, $line) {
-            print $output_file $line;
-        }
-    }
+    # UTF-8 Check
+    use Encode::Guess;
+    open my $target_file, '<', $target_file_path or die "Cannot open target file: $!";
+    my $first_few_bytes;
+    read($target_file, $first_few_bytes, 512) or die "Cannot read from file: $!";
     close $target_file;
+
+    my $decoder = guess_encoding($first_few_bytes);
+    if (ref($decoder)) {
+        if ($decoder->name eq 'utf8') {
+            # Open the file only when the encoding is utf8
+            open $target_file, '<:encoding(UTF-8)', $target_file_path or die "Cannot open target file: $!";
+            while (my $line = <$target_file>) {
+                if (grep { $dictionary{lc $_} } split /[\W_]+/u, $line) {
+                    print $output_file $line;
+                }
+            }
+            close $target_file;
+        } else {
+            die "Target file appears to be in " . $decoder->name . " format, not UTF-8\n";
+        }
+    } else {
+        die "Cannot guess the encoding of target file: $decoder\n";
+    }
 
     close $output_file;
     print "Output file written to $output_file_path\n";
 }
+
 
 my $start_time = time();
 
