ee571a64e4c92d0b06346c2aa3ebd6aa4afa3ed5
Jay Seo <15714852+jpseo84@users.noreply.github.com>
2025-04-25 08:00:57 +0900

    Fixed minor error

--- filemerger.pl
+++ filemerger.pl
@@ -1,6 +1,6 @@
 ###########################################################################
-# File Merger (encconvert) 0.1                                #
-# Copyright (C) 2024 Jupyo Seo                                            #
+# File Merger (encconvert) 0.2                                #
+# Copyright (C) 2025 Jupyo Seo                                            #
 # This program is free software; you can redistribute it and/or modify    #
 # it under the terms of the GNU General Public License as published by    #
 # the Free Software Foundation; either version 2 of the License, or       #
